3823496da64cda8a45dec963e22a95494c75a0f8
Owen <wheeleow@mail.gvsu.edu>
2023-12-01 13:42:11 -0500

    initial commit

---
+++ math_functions.py
@@ -0,0 +1,13 @@
+def add_numbers(a, b):
+	return a + b
+def subtract_numbers(a, b):
+	return a - b
+if __name__ == "__main__":
+	print("Adding:", add_numbers(2,4))
+	print("Subtracting:", subtract_numbers(9,2))
+	print("multiplying:", multiply_numbers(3,4))
+	print("dividing:", divide_numbers(10,5))
+def multiply_numbers(a,b):
+	return a*b
+def divide_numbers(a,b):
+	return a/b
